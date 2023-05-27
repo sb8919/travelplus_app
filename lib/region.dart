fde71d370b2fb92f1a521f26e5d16a94176a3e16
J  W <xmsxmsxpsxps@Naver.com>
2023-05-27 18:50:08 +0900

    관심지역 클래스에서 지역들 세부추가

--- lib/region.dart
+++ lib/region.dart
@@ -16,9 +16,166 @@ class _RegionState extends State<Region> {
   String? selectedRegion;
   String? selectedCity;
 
-  List<String> seoulCities = ['강남구', '강북구', '강서구'];
-  List<String> gyeonggiCities = ['수원', '용인', '성남'];
-  // 다른 권역과 해당 도시를 추가할 수 있습니다.
+  List<String> seoulCities = [
+    '강남구',
+    '강북구',
+    '강서구',
+    '관악구',
+    '광진구',
+    '구로구',
+    '금천구',
+    '노원구',
+    '도봉구',
+    '동대문구',
+    '동작구',
+    '마포구',
+    '서대문구',
+    '서초구',
+    '성동구',
+    '성북구',
+    '송파구',
+    '양천구',
+    '영등포구',
+    '용산구',
+    '은평구',
+    '종로구',
+    '중구',
+    '중랑구',
+  ];
+  List<String> gyeonggiCities = [
+    '수원',
+    '용인',
+    '성남',
+    '부천',
+    '안산',
+    '안양',
+    '남양주',
+    '화성',
+    '평택',
+    '의정부',
+    '시흥',
+    '파주',
+    '광명',
+    '김포',
+    '군포',
+    '이천',
+    '양주',
+    '오산',
+    '구리',
+    '안성',
+    '포천',
+    '의왕',
+    '하남',
+    '여주',
+  ];
+  List<String> gangwonCities = [
+    '춘천',
+    '원주',
+    '강릉',
+    '속초',
+    '동해',
+    '태백',
+    '홍천',
+    '횡성',
+    '영월',
+    '평창',
+    '정선',
+    '철원',
+    '화천',
+    '양구',
+    '인제',
+    '고성',
+    '양양',
+  ];
+  List<String> chungcheongCities = [
+    '청주',
+    '대전',
+    '충주',
+    '제천',
+    '보령',
+    '천안',
+    '공주',
+    '아산',
+    '서산',
+    '논산',
+    '계룡',
+    '당진',
+    '금산',
+    '부여',
+    '서천',
+    '청양',
+    '홍성',
+    '예산',
+    '태안',
+  ];
+  List<String> gyeongsangCities = [
+    '부산',
+    '대구',
+    '울산',
+    '창원',
+    '포항',
+    '경주',
+    '김천',
+    '안동',
+    '구미',
+    '경산',
+    '의성',
+    '청송',
+    '상주',
+    '문경',
+    '영주',
+    '영천',
+    '군위',
+    '고령',
+    '성주',
+    '칠곡',
+    '예천',
+    '봉화',
+    '울릉',
+    '독도',
+  ];
+  List<String> jeollaCities = [
+    '광주',
+    '전주',
+    '여수',
+    '순천',
+    '남원',
+    '익산',
+    '목포',
+    '군산',
+    '정읍',
+    '남해',
+    '고흥',
+    '보성',
+    '화순',
+    '장흥',
+    '강진',
+    '해남',
+    '영암',
+    '무안',
+    '함평',
+    '영광',
+    '장성',
+    '완도',
+    '진도',
+    '신안',
+  ];
+  List<String> jejuCities = [
+    '제주시',
+    '서귀포',
+    '애월',
+    '한림',
+    '중문',
+    '성산',
+    '조천',
+    '구좌',
+    '남원',
+    '표선',
+    '송산',
+    '안덕',
+    '대정',
+    '한경',
+  ];
 
   List<DropdownMenuItem<String>> generateRegionItems() {
     List<String> regions = ['서울', '경기', '강원', '충청', '경상', '전라', '제주'];
@@ -39,7 +196,21 @@ class _RegionState extends State<Region> {
       case '경기':
         cities = gyeonggiCities;
         break;
-    // 다른 권역에 대한 도시 목록을 추가합니다.
+      case '강원':
+        cities = gangwonCities;
+        break;
+      case '충청':
+        cities = chungcheongCities;
+        break;
+      case '경상':
+        cities = gyeongsangCities;
+        break;
+      case '전라':
+        cities = jeollaCities;
+        break;
+      case '제주':
+        cities = jejuCities;
+        break;
     }
     return cities.map((city) {
       return DropdownMenuItem<String>(

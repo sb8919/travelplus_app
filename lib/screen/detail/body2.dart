ab9ec38c262240bdfbabff692c3e254812bbb4b4
sb8919 <sb8919@naver.com>
2023-06-05 17:24:29 +0900

    지도화면 UI변경

--- lib/screen/detail/body2.dart
+++ lib/screen/detail/body2.dart
@@ -118,37 +118,100 @@ class _Body2State extends State<Body2> {
         Expanded(
           flex: 3,
           child: ListView.builder(
+            scrollDirection: Axis.horizontal,
             itemCount: places.length,
             itemBuilder: (context, index) {
               final place = places[index];
-              return ListTile(
-                title: Text(place[0]),
-                leading: Image.network(
-                  place[1],
-                  loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                          : null,
-                    );
-                  },
-                  errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
-                    return Icon(Icons.error);
-                  },
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 390,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 15.0,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: GestureDetector(
+                        onTap: (){},
+                        child: Material(
+                          borderRadius: BorderRadius.circular(8.0),
+                          elevation: 0.0,
+                          color: Colors.white, // 배경색을 흰색으로 설정
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: 150,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(8.0),
+                                  ),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(8.0),
+                                  ),
+                                  child: Image.network(
+                                    place[1],
+                                    loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                                      if (loadingProgress == null) return child;
+                                      return CircularProgressIndicator(
+                                        value: loadingProgress.expectedTotalBytes != null
+                                            ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                                            : null,
+                                      );
+                                    },
+                                    errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
+                                      return Icon(Icons.error);
+                                    },
+                                    fit: BoxFit.cover,
+                                    width: double.infinity,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  place[0],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  place[2],
+                                  style: TextStyle(fontSize: 14.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: IconButton(
+                        icon: Icon(Icons.favorite),
+                        color: Colors.red,
+                        onPressed: () {
+                          // Add your logic here
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-                subtitle: Text(place[2]),
-                onTap: () {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                  final double latitude = double.parse(place[4]);
-                  final double longitude = double.parse(place[5]);
-                  mapController.move(
-                    LatLng(latitude, longitude),
-                    16.0,
-                  );
-                },
               );
             },
           ),
@@ -156,6 +219,7 @@ class _Body2State extends State<Body2> {
       ],
     );
   }
+
 
   List<Marker> _buildMarkers() {
     List<Marker> markers = [];
